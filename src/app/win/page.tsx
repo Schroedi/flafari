2b8d3015920a67284934ccbf4a7243d7ac8e950b
Christoph Schröder <cs@keksdev.de>
2024-11-09 20:07:51 +0100

    fix: win screen layout

--- src/app/win/page.tsx
+++ src/app/win/page.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useState } from "react";
 import Polaroid, { PolaroidFrame } from "../../components/Polaroid";
 import Score from "@/components/Score";
 import ImageBlender from "@/components/ImageBlender";
+import { Share } from "lucide-react";
 
 export default function WinPage() {
 	const [loadedImage, setLoadedImage] = useState<string | null>(null);
@@ -54,19 +55,12 @@ export default function WinPage() {
 
 	return (
 		<div className="">
-			<div className="flex flex-col items-center h-screen bg-gradient-to-br from-purple-700 to-pink-500">
-				<h1 className="text-center text-4xl font-bold text-white mt-10">
+			<div className="flex flex-col justify-between items-center h-screen bg-gradient-to-br from-purple-700 to-pink-500">
+				<h1 className="text-center text-4xl font-bold text-white mt-8">
 					You did it!
 				</h1>
-				<button
-					type="button"
-					onClick={doShare}
-					className="bg-white text-purple-700 px-4 py-2 rounded-full inline-block transform -rotate-2"
-				>
-					Share
-				</button>
 
-				<div className="relative mt-4">
+				<div className="relative mt-0">
 					{/* {loadedImage && loadedUserImage && (
 						<>
 							<div className="absolute inset-0 transition-opacity duration-1000 ease-in-out animate-fade-in-out">
@@ -86,6 +80,13 @@ export default function WinPage() {
 				{isClientMounted && (
 					<Score className="transform -translate-y-1/2 translate-x-1/3" />
 				)}
+				<button
+					type="button"
+					onClick={doShare}
+					className="fixed bottom-4 left-1/2 -translate-x-1/2 -translate-y-10 bg-white text-purple-700 px-4 py-2 rounded-full inline-block transform -rotate-2"
+				>
+					Share <Share className="inline-block ml-1" size={16} />
+				</button>
 			</div>
 		</div>
 	);
