958298baed2d25f3d64a9a254315616719d83417
Christoph Schröder <cs@keksdev.de>
2024-11-10 16:41:45 +0100

    feat: redirect to mobile phone on pc

--- src/components/ScareAlert.tsx
+++ src/components/ScareAlert.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import FlashyButton from "./Startbutton";
 import { useRouter } from "next/navigation";
+import QRCode from "react-qr-code";
 
 export default function ImpressumDialog() {
 	const router = useRouter();
@@ -27,27 +28,33 @@ export default function ImpressumDialog() {
 					<AlertDialogTitle>Kamera Zugriff</AlertDialogTitle>
 					<AlertDialogDescription className="" asChild>
 						<div className="">
-							<p>
-								Um das Spiel zu spielen, benötigen wir Zugriff auf die Kamera
-								des Geräts.
-							</p>
-
-							<h3 className="font-bold mt-4">
-								Alle Daten werden lokal gespeichert!
-							</h3>
-							<p>
-								Die Daten werden ausschließlich lokal auf deinem Gerät
-								gespeichert und nicht an uns oder andere übertragen.
-							</p>
+							<div className="hidden md:block">
+								<p>Dieses Spiel funktioniert nur auf dem Handy.</p>
+								<QRCode
+									className="w-full h-auto pt-4"
+									value="https://flascherazzi.de"
+								/>
+							</div>
+							<div className="md:hidden">
+								<p>
+									Um das Spiel zu spielen, benötigen wir Zugriff auf die Kamera
+									des Geräts.
+								</p>
+								<h3 className="font-bold mt-4">
+									Alle Daten werden lokal gespeichert!
+								</h3>
+								<p>
+									Die Daten werden ausschließlich lokal auf deinem Gerät
+									gespeichert und nicht an uns oder andere übertragen.
+								</p>
+							</div>
 						</div>
 					</AlertDialogDescription>
 				</AlertDialogHeader>
 				<AlertDialogFooter>
-					<AlertDialogCancel className="mx-10">
-						Nein, lieber nicht
-					</AlertDialogCancel>
+					<AlertDialogCancel className="mx-10">Zurück</AlertDialogCancel>
 					<AlertDialogAction
-						className="mx-10"
+						className="mx-10 md:hidden"
 						onClick={() => router.push("/cam")}
 					>
 						Los geht's!
